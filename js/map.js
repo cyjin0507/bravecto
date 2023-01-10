ab67bc9470389e1291591d92feb33ba4784d1a60
choiyoungjin <82877643+cyjin0507@users.noreply.github.com>
2023-01-10 13:53:14 +0900

    c

--- js/map.js
+++ js/map.js
@@ -50,25 +50,86 @@ class map {
             let lat = parseFloat(data[i].location.split(',')[0].replaceAll('(', '').replaceAll(' ', ''))
             let lng = parseFloat(data[i].location.split(',')[1].replaceAll(")", '').replaceAll(' ', ''))
 
-            if(bool) {
-                marker = new google.maps.Marker({
-                    position: { lat, lng },
-                    i,
-                    map,
-                    icon: { url: "//maps.google.com/mapfiles/ms/icons/blue-dot.png" }
-                });
-            } else {
-                marker = new google.maps.Marker({
-                    position: { lat, lng },
-                    i,
-                    map
-                });
-            }
-
-            
+            marker = new google.maps.Marker({
+                position: { lat, lng },
+                i,
+                map
+            });
 
             let infowindow = new google.maps.InfoWindow();
             infoPop.push(infowindow)
+
+            if(bool) {
+                infoPop.forEach(e => {
+                    e.close()
+                })
+                if (option1 == 1) {
+                    infowindow.setContent(`
+                        <div class="mapInfo">
+                            <div class="info">
+                                <div>업체명</div>
+                                <div>${name}</div>
+                            </div>
+                            <div class="info">
+                                <div>주소</div>
+                                <div>${road}</div>
+                            </div>
+                            <div class="info">
+                                <div>연락처</div>
+                                <div>${phone == 'undefined' ? '-' : phone}</div>
+                            </div>
+                            <div class="icons">
+                                <div>
+                                    <img src="./settings/imgs/dog_icon.png" />
+                                    <img src="./settings/imgs/cat_icon.png" />
+                                </div>
+                            </div>
+                            <div class="btnGroup">
+                                <button class="ok">지도보기</button>
+                                <button class="ok">확인</button>
+                            </div>
+                        </div>
+                    `);
+                } else if (option1 == 0) {
+                    infowindow.setContent(`
+                        <div class="mapInfo">
+                            <div class="info">
+                                <div>업체명</div>
+                                <div>${name}</div>
+                            </div>
+                            <div class="info">
+                                <div>주소</div>
+                                <div>${road}</div>
+                            </div>
+                            <div class="info">
+                                <div>연락처</div>
+                                <div>${phone == 'undefined' ? '-' : phone}</div>
+                            </div>
+                            <div class="icons">
+                                <img src="./settings/imgs/dog_icon.png" />
+                            </div>
+                            <div class="btnGroup">
+                                <button>지도보기</button>
+                                <button>확인</button>
+                            </div>
+                        </div>
+                    `);
+                }
+
+                
+
+                setTimeout(()=> {
+                    $('.ok').click(()=> {
+                        infoPop.forEach(e => {
+                            e.close()
+                        })
+                    })
+                },100)
+
+                infowindow.open(map, marker);
+            }
+
+
             google.maps.event.addListener(marker, 'click', (function (marker, i) {
                 return function () {
 
